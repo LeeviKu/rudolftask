e7b50fc4ee079d56be0feec3f2a845dbdae6ef33
LeeviKu <leevi.kukkula@gmail.com>
2021-04-21 16:07:23 +0300

    Add config

---
+++ config.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql')
+require('dotenv').config()
+
+module.exports = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
+})
